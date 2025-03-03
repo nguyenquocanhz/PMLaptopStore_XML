e0d94587c79915555247c0a3b5fb75d512257df1
Nguyen Quoc Anh <nguyenquocanh.dev@gmail.com>
2025-03-03 14:34:37 +0700

    Add project files.

---
+++ fQuanLySanPham.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PMLaptopStore_XML
+{
+    public partial class fQuanLySanPham: Form
+    {
+        public fQuanLySanPham()
+        {
+            InitializeComponent();
+        }
+    }
+}
